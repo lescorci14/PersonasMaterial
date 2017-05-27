ba430c9095c9fcfaa0c558106dd3458f61fd5daf
Leiner Escorcia <leiner_0304@hotmail.com>
2017-05-27 17:48:26 -0500

    Terminado

--- app/src/main/java/com/example/cuc/personasmaterial/DetallePersona.java
+++ app/src/main/java/com/example/cuc/personasmaterial/DetallePersona.java
@@ -1,13 +1,42 @@
 package com.example.cuc.personasmaterial;
 
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class DetallePersona extends AppCompatActivity {
-
+   private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Persona p;
+    private String nombre,apellido,urlfoto;
+    private Bundle b;
+    private Intent i;
+    private ImageView foto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_persona);
+
+        Toolbar toolbar= (Toolbar)findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        i = getIntent();
+        b=i.getBundleExtra("datos");
+        nombre = b.getString("nombre");
+        apellido = b.getString("apellido");
+        urlfoto = b.getString("urlfoto");
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+        foto = (ImageView)findViewById(R.id.fotoPersona);
+
+        Picasso.with(getApplicationContext()).load(urlfoto).into(foto);
+        collapsingToolbarLayout.setTitle(nombre+" "+apellido);
+
     }
 }
--- app/src/main/java/com/example/cuc/personasmaterial/Principal.java
+++ app/src/main/java/com/example/cuc/personasmaterial/Principal.java
@@ -1,28 +1,23 @@
 package com.example.cuc.personasmaterial;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 
-public class Principal extends AppCompatActivity implements AdaptadorPersona.OnPersonaClickListener{
+public class Principal extends AppCompatActivity implements AdaptadorPersona.OnPersonaClickListener {
     private RecyclerView listado;
     private ArrayList<Persona> personas;
     private AdaptadorPersona adapter;
     private LinearLayoutManager llm;
-    private Resources res;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +26,7 @@ public class Principal extends AppCompatActivity implements AdaptadorPersona.OnP
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        listado = (RecyclerView)findViewById(R.id.lstOpciones);
+        listado = (RecyclerView) findViewById(R.id.lstOpciones);
 
         personas = Datos.traerPersonas(getApplicationContext());
         adapter = new AdaptadorPersona(personas,this);
@@ -52,5 +47,14 @@ public class Principal extends AppCompatActivity implements AdaptadorPersona.OnP
 
     @Override
     public void onPersonaClick(Persona p) {
+        //finish();
+        Intent i = new Intent(Principal.this,DetallePersona.class);
+        Bundle b = new Bundle();
+        b.putString("nombre",p.getNombre());
+        b.putString("apellido",p.getApellido());
+        b.putString("urlfoto",p.getUrlfoto());
+
+        i.putExtra("datos",b);
+        startActivity(i);
     }
 }
